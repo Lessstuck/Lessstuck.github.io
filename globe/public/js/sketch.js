b1ce1640df4faff591b2433f386ac47381977bde
Les Stuck <les@lesstuck.com>
2018-06-16 09:45:00 -0700

    delete css

--- globe/public/js/sketch.js
+++ globe/public/js/sketch.js
@@ -40,51 +40,36 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // Append Renderer to DOM
 console.log('renderer: ', renderer.domElement);
 document.body.appendChild(renderer.domElement);
-/*
-var spotLight = new THREE.SpotLight(0xdddddd);
-spotLight.position.set(-0, 30, 60);
-spotLight.castShadow = true;
-spotLight.intensity = 1.0;
-scene.add(spotLight);
-*/
+
+
 scene.add( new THREE.AmbientLight( 0xffffff, 2 ) );
-			/*	var directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
-				directionalLight.position.x = Math.random() - 0.5;
-				directionalLight.position.y = Math.random() - 0.5;
-				directionalLight.position.z = Math.random() - 0.5;
-				directionalLight.position.normalize();
-				scene.add( directionalLight );
-				pointLight = new THREE.PointLight( 0xffffff, 2 );
-				scene.add( pointLight );
-*/
 var textureLoader = new THREE.TextureLoader();
 var earthTexture = textureLoader.load( './models/Earth_TEXTURE_CM.jpg' );
-
+//earthTexture.mapping = THREE.EquirectangularRefractionMapping;
 var earthBall = new THREE.OBJLoader();
+earthBall.setPath( './models/' );
+earthBall.load( 'Earth.obj', function ( object ) {
+    scene.add( object );
+    object.scale.set(.25,.25,.25);
+})
+
+//earthBall.scale=(.5,.5,.5);
+
 var earthMat = new THREE.MTLLoader();
 earthMat.setPath( './models/' );
 earthMat.load( "Earth.mtl", function( materials ) {
     materials.preload();
     earthBall.setMaterials( materials );
-    earthBall.setPath( './models/' );
-    earthBall.load( 'Earth.obj', function ( object ) {
-        scene.add( object );
-        object.scale.set(1,1,1);
-  },
-  // called when loading is in progresses
-  function ( xhr ) {
-    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-  },
-  function ( error ) {
-    console.log( 'An error happened' );
-  })});
-//  earthBall.scale.set(.25,.25,.25);
+  });
+
+
 //scene.add(cube);
 
 // Render Loop
 var render = function() {
   requestAnimationFrame(render);
-
+  //earthBall.rotation.x += (0.2*(Math.PI / 180));
+  //earthBall.rotation.x %=360;
 /*
   cube.rotation.x = 1;
   cube.rotation.y = 1;
