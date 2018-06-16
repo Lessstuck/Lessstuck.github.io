2ec1cd9f27bc9d8393284410eb574fdaf319fe74
Les Stuck <les@lesstuck.com>
2018-06-15 23:48:51 -0700

    working world, with jpg texture, mtl loader, yeah!

--- globe/public/js/sketch.js
+++ globe/public/js/sketch.js
@@ -29,86 +29,67 @@ socket.on('error', function() {
 });
 
 
-// part 2 of scipt from index
 var scene = new THREE.Scene();
-
-// Create a basic perspective camera
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
-
-// Create a renderer with Antialiasing
 var renderer = new THREE.WebGLRenderer({
   antialias: true
 });
-
-// Configure renderer clear color
 renderer.setClearColor("#000000");
-
-// Configure renderer size
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 // Append Renderer to DOM
 console.log('renderer: ', renderer.domElement);
 document.body.appendChild(renderer.domElement);
-
-
-// ------------------------------------------------
-// FUN STARTS HERE
-// ------------------------------------------------
-
-//Add a light
-var spotLight = new THREE.SpotLight(0xffffff);
+/*
+var spotLight = new THREE.SpotLight(0xdddddd);
 spotLight.position.set(-0, 30, 60);
 spotLight.castShadow = true;
-spotLight.intensity = 0.6;
+spotLight.intensity = 1.0;
 scene.add(spotLight);
+*/
+scene.add( new THREE.AmbientLight( 0xffffff, 2 ) );
+			/*	var directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
+				directionalLight.position.x = Math.random() - 0.5;
+				directionalLight.position.y = Math.random() - 0.5;
+				directionalLight.position.z = Math.random() - 0.5;
+				directionalLight.position.normalize();
+				scene.add( directionalLight );
+				pointLight = new THREE.PointLight( 0xffffff, 2 );
+				scene.add( pointLight );
+*/
+var textureLoader = new THREE.TextureLoader();
+var earthTexture = textureLoader.load( './models/Earth_TEXTURE_CM.jpg' );
 
-// Create a Cube Mesh with basic material
-var geometry = new THREE.BoxGeometry(2, 2, 2);
-var material = new THREE.MeshStandardMaterial({color: 0x7777ff})
-var cube = new THREE.Mesh(geometry, material);
-
-// instantiate a loader
-var loader = new THREE.OBJLoader();
-var mtlLoader = new THREE.MTLLoader();
-
-// load a resource
-loader.load(
-  // resource URL
-  './models/Earth.obj',
-  // called when resource is loaded
-  function ( object ) {
-
-    scene.add( object );
-
+var earthBall = new THREE.OBJLoader();
+var earthMat = new THREE.MTLLoader();
+earthMat.setPath( './models/' );
+earthMat.load( "Earth.mtl", function( materials ) {
+    materials.preload();
+    earthBall.setMaterials( materials );
+    earthBall.setPath( './models/' );
+    earthBall.load( 'Earth.obj', function ( object ) {
+        scene.add( object );
+        object.scale.set(1,1,1);
   },
   // called when loading is in progresses
   function ( xhr ) {
-
     console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
   },
-  // called when loading has errors
   function ( error ) {
-
     console.log( 'An error happened' );
-
-  }
-);
-
-
-// Add cube to Scene
-scene.add(cube);
+  })});
+//  earthBall.scale.set(.25,.25,.25);
+//scene.add(cube);
 
 // Render Loop
 var render = function() {
   requestAnimationFrame(render);
 
-  cube.rotation.x = a;
+/*
+  cube.rotation.x = 1;
   cube.rotation.y = 1;
   cube.rotation.z = 1;
-
-  // Render the scene
+*/
   renderer.render(scene, camera);
 };
 
