c2acf50b716f99ba82df08539fd2d3a708bdfe52
Les Stuck <les@lesstuck.com>
2020-06-28 12:47:13 -0600

    moved all js from inline into app.js

--- noneuclidean/app.js
+++ noneuclidean/app.js
@@ -2,3 +2,33 @@ import { Track } from "./modules/noneuclidean.mjs";
 
 const track = new Track([.25, .25, .5]);
 track.play();
+
+{/* <script type="text/javascript">; */}
+
+            function play_sounds()  {
+
+        play_sound1();
+                play_sound2();
+                play_sound3();
+                play_sound4();
+                console.log("play_sounds");
+            };
+            function play_sound1()  {
+        document.getElementById('sound1').play();
+            };
+            function play_sound2() {
+        document.getElementById('sound2').play();
+            };
+            function play_sound3() {
+        document.getElementById('sound3').play();
+            };
+            function play_sound4() {
+        document.getElementById('sound4').play();
+            };
+            function startBeat()    {
+        beat = setInterval(play_sounds, 1000);
+            };
+            function stopBeat() {
+        clearInterval(beat);
+            };
+        {/* </script> */}
